e0dfb03737d5ae0cee10603fd2b75a503505e7f7
irdomeriko <109429312+irdomeriko@users.noreply.github.com>
2024-01-11 18:14:20 +0100

    Calc 2.0

--- Restar.py
+++ Restar.py
@@ -1,13 +1,29 @@
+#importamos la libreria time
 import time
+#Variable = """Instrucciones"""
+g = """
+Instrucciones : 
+El argumento A es el primer argumento
+Y el Argumento B es el segundo argumento
+Ejemplo
+Argumento a 5
+Argumento b 2
+Ejemplo Argumento a - Argumento b       5-2
+Respuesta = 3
+"""
+#print(f"Instrucciones : "(Variavle de Instrucciones))
+print(g)
+time.sleep(3)
 # variable = int(input("Texto"))
 a = int(input("Argumento a "))
 b = int(input("Argumento b "))
 # variable = variable - variable
-c = a - b
+c = (a - b)
 #print(variable)
 print(c)
 #print("Authors")
 print("By Irdomeriko")
 #time.sleep(seg)
 time.sleep(3)
+#exit
 exit()
--- Sumar.py
+++ Sumar.py
@@ -1,4 +1,17 @@
 import time
+g = """
+Instrucciones : 
+El argumento A es el primer argumento
+Y el Argumento B es el segundo argumento
+Ejemplo
+Argumento a 5
+Argumento b 2
+Ejemplo Argumento a - Argumento b       5-2
+Respuesta = 3
+"""
+print(g)
+time.sleep(3)
+
 a = int(input("Argumento a "))
 b = int(input("Argumento b "))
 c = a + b
