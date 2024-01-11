ab4367bd68c2a1f0b18f8bd8bbe8725536040332
irdomeriko <109429312+irdomeriko@users.noreply.github.com>
2024-01-11 08:39:41 +0100

    Calculadora

---
+++ Restar.py
@@ -0,0 +1,13 @@
+import time
+# variable = int(input("Texto"))
+a = int(input("Argumento a "))
+b = int(input("Argumento b "))
+# variable = variable - variable
+c = a - b
+#print(variable)
+print(c)
+#print("Authors")
+print("By Irdomeriko")
+#time.sleep(seg)
+time.sleep(3)
+exit()
---
+++ Sumar.py
@@ -0,0 +1,9 @@
+import time
+a = int(input("Argumento a "))
+b = int(input("Argumento b "))
+c = a + b
+print(c)
+print("By Irdomeriko")
+
+time.sleep(3)
+exit()
